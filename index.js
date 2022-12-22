fd2a049ae8feab010edf4bebe0c8a9ebb1dc0ccf
Yash Kaskar <yash.kaskar99@gmail.com>
2022-12-22 13:53:14 +0530

    install express

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true)
+
+
+
+
+mongoose.connect('mongodb://localhost:27017/shopApps')
+.then(() =>{
+    console.log(" Connection Open !!!!")
+})
+.catch(err => {
+    console.log("Ohh No Error !!!")
+    console.log(err)
+})
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+
+app.get('/dogs', (req, res) =>{
+    app.send('WOOF');
+})
 
 
 app.listen(8080, () => {
