f1cca326ad63b7fa89c94fab9cdb946ba0415644
Yash Kaskar <yash.kaskar99@gmail.com>
2022-12-23 13:14:48 +0530

    product Schema

--- models/product.js
+++ models/product.js
@@ -1,4 +1,23 @@
 import mongoose from 'mongoose';
 
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+
+    category: {
+        type: String,
+        enum: ['fruits', 'vegetable', 'diary']
+    }
+});
+
+const Product = mongoose.model('Product', productSchema);
 
 module.exports = 'Product'
