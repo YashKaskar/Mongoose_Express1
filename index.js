f6ffb5a6da2690b3519f5e2d98884eb5b9d745c0
Yash Kaskar <yash.kaskar99@gmail.com>
2022-12-23 13:17:35 +0530

    require product

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true)
 
-
-
+const Product = require('./models/product');
 
 mongoose.connect('mongodb://localhost:27017/farmStands')
 .then(() =>{
