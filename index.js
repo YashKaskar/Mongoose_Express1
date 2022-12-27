6889d56612b7c07428cb19e0c40ae4bd82ed42d4
Yash Kaskar <yash.kaskar99@gmail.com>
2022-12-27 13:36:46 +0530

    new route for new product

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ mongoose.connect('mongodb://localhost:27017/farmStands')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.get('/products/new', (req, res) => {    
+res.render('products/new')
+})
 
 app.get('/products',async (req, res) => {
     const products = await Product.find({})
