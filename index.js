841ccb9b2da77b655915eb311c036ff85314c318
Yash Kaskar <yash.kaskar99@gmail.com>
2022-12-28 11:42:36 +0530

    changes

--- index.js
+++ index.js
@@ -52,6 +52,12 @@ app.get('/products/:id/edit', async (req, res) => {
     res.render('products/edit', {product})
 })
 
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })  
+    res.redirect(`/products/${product._id}`)
+})
+
 
 app.listen(8080, () => {
         console.log('APP IS LISTENING ON PORT 8080')
