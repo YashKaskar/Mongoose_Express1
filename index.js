56b969b3d170ebc88a51bdd19b4bbf63adee2a96
Yash Kaskar <yash.kaskar99@gmail.com>
2022-12-28 11:30:19 +0530

    send to use

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const methodOverride = require('method-override')
-app.use(methodOverride('_method'))
+
 
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true)
@@ -20,7 +20,9 @@ mongoose.connect('mongodb://localhost:27017/farmStands')
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended: true}));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 
 app.get('/products/new', (req, res) => {    
 res.render('products/new')
