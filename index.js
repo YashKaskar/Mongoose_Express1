1a0e31b7bc4f3ba17b45bff0d8db9db60ed3f0b4
Yash Kaskar <yash.kaskar99@gmail.com>
2022-12-28 11:27:24 +0530

    require method-override

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override')
 
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true)
