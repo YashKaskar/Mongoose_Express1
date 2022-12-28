60f8be51707f7dd1fb43e54f91353e17b7222968
Yash Kaskar <yash.kaskar99@gmail.com>
2022-12-28 11:28:16 +0530

    app .use method

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true)
